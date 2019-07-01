f9a3996bd50177bc7631aaa6835647d363cc6545
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-01 01:21:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array = []
+  array = [1]
   const newArray = [element, ...array]
   return array
 }
