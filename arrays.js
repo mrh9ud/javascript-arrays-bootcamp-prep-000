144ef516ac36aa24cffba800d5b7889c4f0320ac
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-01 01:55:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,10 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-
+function removeElementFromEndOfArray(array) {
+  array.slice(0, array.length - 1)
+  return array
+}
 
 
 
