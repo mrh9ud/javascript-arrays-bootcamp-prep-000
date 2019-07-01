f90ffb75988f1cc474154f107f8208804e744967
Matthew Heavner <mrh9ud@virginia.edu>
2019-07-01 00:55:30 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var ingredient1 = "bread"
+var ingredient2 = "mild cheese"
+var ingredient3 = "sharp cheese"
+var ingredient4 = "butter"
+var ingredient5 = "tomato"
+var ingredient6 = "garlic"
+
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+var myArray = [element0, element1, elementN];
+
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+
+var tvShows = ["game of thrones", "true detective", "the good wife", "empire"];
+
+var weirdGreeting = ["he", 110, "w", 0, "r", 1, "d"];
+
+var empty = [];
+
+console.log(myArray)
